9cae27e1fc0d424b0ddbb38ac9b73427b05b3826
Brayan <bittolo.8bit@gmail.com>
2026-03-02 19:45:01 +0100

    refining searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,10 +5,19 @@ export default function Searchbar({ setResults }) {
   const [query, setQuery] = useState("");
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+  const handleSearch = (e) => {};
+
   return (
     <div className="Searchbar">
-      <input type="text" />
-      <button>Search</button>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Cerca un film o serie..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }

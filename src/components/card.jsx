4eade1c42aef7cf035cc8f0fe31d13128bb93372
Brayan <bittolo.8bit@gmail.com>
2026-03-02 20:09:19 +0100

    styling cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,11 @@
 export default function Card({ item }) {
   return (
     <div className="Card">
+      <img
+        className="Poster"
+        src={`https://image.tmdb.org/t/p/w342${item.poster_path}`}
+        alt={item.title || item.name}
+      />
       <p>{item.title || item.name}</p>
       <p>{item.original_title || item.original_name}</p>
       <p>{item.original_language}</p>

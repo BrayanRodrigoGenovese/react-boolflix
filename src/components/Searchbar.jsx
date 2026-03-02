a49ee2313211c1a687619f2036df03602e445973
Brayan <bittolo.8bit@gmail.com>
2026-03-02 19:58:15 +0100

    adding searchbar logic

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,8 +4,34 @@ import axios from "axios";
 export default function Searchbar({ setResults }) {
   const [query, setQuery] = useState("");
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const BASE_URL = "https://api.themoviedb.org/3";
 
-  const handleSearch = (e) => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (query.trim() === "") return;
+
+    const axiosConfig = {
+      params: {
+        api_key: API_KEY,
+        language: "it-IT",
+        query: query,
+      },
+    };
+
+    Promise.all([
+      axios.get(`${BASE_URL}/search/movie`, axiosConfig),
+      axios.get(`${BASE_URL}/search/tv`, axiosConfig),
+    ])
+      .then(([moviesResponse, tvShowsResponse]) => {
+        setResults([
+          ...moviesResponse.data.results,
+          ...tvShowsResponse.data.results,
+        ]);
+      })
+      .catch((error) => {
+        console.error("Errore durante la ricerca:", error);
+      });
+  };
 
   return (
     <div className="Searchbar">
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,10 @@
-export default function Card() {
+export default function Card({ item }) {
   return (
     <div className="Card">
-      <p>{"Title"}</p>
-      <p>{"OriginalTitle"}</p>
-      <p>{"Language"}</p>
-      <p>{"Rating"}</p>
+      <p>{item.title || item.name}</p>
+      <p>{item.original_title || item.original_name}</p>
+      <p>{item.original_language}</p>
+      <p>{item.overview || "Nessuna descrizione disponibile."}</p>
     </div>
   );
 }
